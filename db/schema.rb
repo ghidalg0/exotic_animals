22ef0079b64adf70ddbbfbebda07907fbce1c191
Rayane Moula <moula.rayane@gmail.com>
2022-05-23 20:38:33 +0200

    add column in users table: nickname firstname lastname address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_142902) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_182957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_142902) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
