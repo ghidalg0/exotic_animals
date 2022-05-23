4ae93f8d1833eecd575724e4cebda0db37c4da99
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-05-23 17:11:25 +0200

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "animals#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :animals, only: %i[index show new create] do
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[delete]
 end
