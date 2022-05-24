a0d2f072a82c699c7210835d954302f2caf754e1
Rayane Moula <moula.rayane@gmail.com>
2022-05-24 12:32:28 +0200

    basics validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :animal
+
+  validates :status, presence: true, inclusion: { in: %w(Accepted Rejected Peding) }
+  validates :date, presence: true
 end
