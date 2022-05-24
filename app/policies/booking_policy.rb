762edd127cf06ac34ff8106682a286f19e36e28a
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-05-24 17:22:11 +0200

    pundit over

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,10 +1,11 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
+
   def accept_booking?
     record.animal.user == user
   end
@@ -12,4 +13,17 @@ class BookingPolicy < ApplicationPolicy
   def reject_booking?
     record.animal.user == user
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+
 end
