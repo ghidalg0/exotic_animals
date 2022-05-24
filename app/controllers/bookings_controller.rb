ceb5e603704fdec322ccd70a406741bd4cf12bfd
patrick njoume <p.njoume@gmail.com>
2022-05-24 11:23:11 +0200

    controller_bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,8 +8,8 @@ class BookingsController < ApplicationController
     @animal = Animal.find(params[:animal_id])
     @booking = Booking.new(booking_params)
     @booking.animal = @animal
-    @booking.user = @current_user
-    if @booking.save
+    @booking.user = current_user
+    if @booking.save!
       redirect_to new_animal_booking_path(@booking)
     else
       render :new, status: :unprocessable_entity
@@ -24,6 +24,6 @@ class BookingsController < ApplicationController
 private
 
   def booking_params
-    params.require(:booking).permit(:date)
+    params.require(:booking).permit(:date, :animal_id)
   end
 end
