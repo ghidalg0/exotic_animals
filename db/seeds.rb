e638af5a77eed63f63a0dd6fe052329b6bbf7f77
Alex Dereu <adereu08@gmail.com>
2022-05-25 14:47:21 +0200

    seed v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,43 +25,43 @@ lancelot = URI.open("https://i.skyrock.net/2404/61082404/pics/3313490386_1_2_Y0S
 #### user and animal creations
 user_01 = User.create!([{ email: "guilhem@lewagon.fr", password: "azerty", nickname: "Gigi", first_name: "Guilhem", last_name: "Lewagon", address: "Paris" }])
 ####
-animal_0l01 = Animal.new({ name: "Roro", species: "llama", location: "Thoiry", category: "funny", birthday: Date.new(2019, 12, 03), bio: "I love hugs !", user_id: User.last.id })
-animal_0l01.photos.attach(io: roro, filename: "llama")
+animal_0l01 = Animal.new({ name: "Roro", species: "llama", location: "Thoiry", category: "funny", birthday: Date.new(2019, 12, 03), bio: "I love hugs ! I hate to wait ! And food should be good !", user_id: User.last.id })
+animal_0l01.photo.attach(io: roro, filename: "llama")
 animal_0l01.save!
 ####
-animal_0l02 = Animal.new({ name: "Ali", species: "camel", location: "Romagne", category: "funny", birthday: Date.new(2019, 12, 03), bio: "I love making jokes !", user_id: User.last.id })
-animal_0l02.photos.attach(io: ali, filename: "camel")
+animal_0l02 = Animal.new({ name: "Ali", species: "camel", location: "Romagne", category: "funny", birthday: Date.new(2019, 12, 03), bio: "I love making jokes ! I come from Cuba and i love Mojito !", user_id: User.last.id })
+animal_0l02.photo.attach(io: ali, filename: "camel")
 animal_0l02.save!
 
 user_02 = User.create!([{ email: "alex@lewagon.fr", password: "azerty", nickname: "Al", first_name: "Alex", last_name: "Lewagon", address: "Paris" }])
-animal_0201 = Animal.new({ name: "Picpic", species: "peacock", location: "Lyon", category: "funny", birthday: Date.new(2017, 10, 03), bio: "Hi,i'm Picpic and I love joles !", user_id: User.last.id })
-animal_0201.photos.attach(io: picpic, filename: "peacock")
+animal_0201 = Animal.new({ name: "Picpic", species: "peacock", location: "Lyon", category: "funny", birthday: Date.new(2017, 10, 03), bio: "Hi,i'm Picpic and I love joles ! I drink only water !", user_id: User.last.id })
+animal_0201.photo.attach(io: picpic, filename: "peacock")
 animal_0201.save!
 ####
-animal_0202 = Animal.new({ name: "Martin", species: "hedgehog", location: "Montpellier", category: "capricious", birthday: Date.new(2019, 12, 03), bio: "I want to eat ! NOW !", user_id: User.last.id })
-animal_0202.photos.attach(io: martin, filename: "hedgehog")
+animal_0202 = Animal.new({ name: "Martin", species: "hedgehog", location: "Montpellier", category: "capricious", birthday: Date.new(2019, 12, 03), bio: "I want to eat ! NOW ! food is life and life is food !", user_id: User.last.id })
+animal_0202.photo.attach(io: martin, filename: "hedgehog")
 animal_0202.save!
 
 user_03 = User.create!([{ email: "rayane@lewagon.fr", password: "azerty", nickname: "Rara", first_name: "Rayane", last_name: "Lewagon", address: "Paris" }])
 ####
-animal_0301 = Animal.new({ name: "fifi", species: "crocodile", location: "Mulhouse", category: "grumpy", birthday: Date.new(2020, 12, 03), bio: "I love to sleep !", user_id: User.last.id })
-animal_0301.photos.attach(io: fifi, filename: "crocodile")
+animal_0301 = Animal.new({ name: "fifi", species: "crocodile", location: "Mulhouse", category: "grumpy", birthday: Date.new(2020, 12, 03), bio: "I love to sleep ! I need a big bed with lot of pillows", user_id: User.last.id })
+animal_0301.photo.attach(io: fifi, filename: "crocodile")
 animal_0301.save!
 ####
-animal_0302 = Animal.new({ name: "Willou", species: "chimpanzee", location: "Paris", category: "funny", birthday: Date.new(2018, 12, 03), bio: "i love women !", user_id: User.last.id })
-animal_0302.photos.attach(io: willou, filename: "chimpanzee")
+animal_0302 = Animal.new({ name: "Willou", species: "chimpanzee", location: "Paris", category: "funny", birthday: Date.new(2018, 12, 03), bio: "i love women and ruby but not html so much !", user_id: User.last.id })
+animal_0302.photo.attach(io: willou, filename: "chimpanzee")
 animal_0302.save!
 ####
 animal_0303 = Animal.new({ name: "Richard", species: "buffalo", location: "Amiens", category: "grumpy", birthday: Date.new(2018, 12, 03), bio: "Take care of me if you want me to smile !", user_id: User.last.id })
-animal_0303.photos.attach(io: richard, filename: "buffalo")
+animal_0303.photo.attach(io: richard, filename: "buffalo")
 animal_0303.save!
 
 user_04 = User.create!([{ email: "patrick@lewagon.fr", password: "azerty", nickname: "Pat", first_name: "Patrick", last_name: "Lewagon", address: "Paris" }])
 ####
-animal_0401 = Animal.new({ name: "Dede", species: "reindeer", location: "Montpellier", category: "capricious", birthday: Date.new(2019, 12, 03), bio: "I'm a mannequin", user_id: User.last.id })
-animal_0401.photos.attach(io: dede, filename: "reindeer")
+animal_0401 = Animal.new({ name: "Dede", species: "reindeer", location: "Montpellier", category: "capricious", birthday: Date.new(2019, 12, 03), bio: "I'm a mannequin, take pictures of me please and NOW !", user_id: User.last.id })
+animal_0401.photo.attach(io: dede, filename: "reindeer")
 animal_0401.save!
 ####
-animal_0402 = Animal.new({ name: "Lancelot", species: "golden rooster", location: "Maubeuge", category: "capricious", birthday: Date.new(2019, 12, 03), bio: "I'm the king !", user_id: User.last.id })
-animal_0402.photos.attach(io: lancelot, filename: "golden rooster")
+animal_0402 = Animal.new({ name: "Lancelot", species: "golden rooster", location: "Maubeuge", category: "capricious", birthday: Date.new(2019, 12, 03), bio: "I'm the king ! I need everything on this planet !", user_id: User.last.id })
+animal_0402.photo.attach(io: lancelot, filename: "golden rooster")
 animal_0402.save!
